deb928cf82f8f4552a91f246d237172409dd5238
DESKTOP-5FBE16T\User <ltw070@naver.com>
2025-03-28 10:33:50 +0900

    refactoring

--- Project1/SimilarityChecker.cpp
+++ Project1/SimilarityChecker.cpp
@@ -4,12 +4,14 @@
 class SimilarityChecker {
 public:
 	int getScore(std::string s1, std::string s2) {
-		int len1 = s1.length(), len2 = s2.length();
+		int score = calcLengthScore(s1.length(), s2.length());
+		
+		return score;
+
+	}
+	int calcLengthScore(int len1, int len2)	{
 		if (len1 > len2) std::swap(len1, len2);
-
-		if(len1==len2) return 60;
-
+		if (len1 == len2) return 60;
 		return (1 - (len1 - len2) / len2);
-
 	}
 };
