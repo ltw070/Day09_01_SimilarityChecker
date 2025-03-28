00134e3f033ac1ec8389fd0040bd6ae8157cbd28
DESKTOP-5FBE16T\User <ltw070@naver.com>
2025-03-28 10:28:31 +0900

    feature/length 테스트 완료

--- Project1/SimilarityChecker.cpp
+++ Project1/SimilarityChecker.cpp
@@ -4,6 +4,12 @@
 class SimilarityChecker {
 public:
 	int getScore(std::string s1, std::string s2) {
-		if(s1.length()==s2.length()) return 60;
+		int len1 = s1.length(), len2 = s2.length();
+		if (len1 > len2) std::swap(len1, len2);
+
+		if(len1==len2) return 60;
+
+		return (1 - (len1 - len2) / len2);
+
 	}
 };
--- Project1/main.cpp
+++ Project1/main.cpp
@@ -4,6 +4,10 @@
 TEST(SimilarityChecker, length) {
 	SimilarityChecker app;
 	EXPECT_EQ(60, app.getScore("AAA", "AAA"));
+	EXPECT_EQ(60, app.getScore("ASD", "DSA"));
+	EXPECT_EQ( (int)(1-(int)((5-3)/3)) , app.getScore("AAABB", "BAA"));
+	EXPECT_EQ((int)(1 - (int)((3 - 2) / 2)), app.getScore("AA", "AAE"));
+	EXPECT_EQ(60, app.getScore("AAA", "AAA"));
 }
 
 int main() {
